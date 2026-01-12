08daeeef3b63f9cb9e2c223d5276812de721b984
barsikus007 <barsikus07@gmail.com>
2026-01-12 22:24:45 +0300

    migrate from promtail

--- bin/graceful_start.sh
+++ bin/graceful_start.sh
@@ -2,7 +2,7 @@
 
 docker compose up -d --wait samba
 docker compose up -d --wait traefik organizr lldap authelia nextcloud-aio-mastercontainer
-docker compose up -d --wait grafana loki promtail
+docker compose up -d --wait grafana loki alloy
 docker compose up -d --wait whoami transmission jellyfin tubesync syncthing vaultwarden
 docker compose up -d --wait portainer watchtower glances scrutiny
 docker compose up -d --wait prowlarr flaresolverr radarr sonarr
