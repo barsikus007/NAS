ec37e85b663ec61e7daac8ffaf7d9240d6d41f3a
barsikus007 <barsikus07@gmail.com>
2026-05-05 11:10:11 +0300

    misc updates

--- bin/graceful_start.sh
+++ bin/graceful_start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 docker compose up -d --wait samba
-docker compose up -d --wait traefik organizr lldap authelia nextcloud-aio-mastercontainer
-docker compose up -d --wait grafana loki alloy
+docker compose up -d --wait traefik organizr lldap authelia crowdsec loki nextcloud-aio-mastercontainer
+docker compose up -d --wait grafana alloy
 docker compose up -d --wait whoami transmission jellyfin tubesync syncthing vaultwarden
-docker compose up -d --wait portainer watchtower glances scrutiny
+docker compose up -d --wait portainer whatsupdocker glances scrutiny
 docker compose up -d --wait prowlarr flaresolverr radarr sonarr
 # wait for low load
 docker compose up -d --wait gitlab rustdesk-hbbr rustdesk-hbbs
+docker compose up -d --wait notes
